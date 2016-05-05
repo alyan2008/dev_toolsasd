052b5983bc083bb877aecda4cd79a800b75a12cf
alyan2008 <alyan2008@mail.ru>
2016-05-05 18:59:02 +0300

    Update local_env.py

--- local_env.py
+++ local_env.py
@@ -47,16 +47,12 @@ def makeSecret(password):
 
 if args.action == 'create_env':
 	os.system("sudo ntpdate -s ntp.ubuntu.com")
-	os.system("sudo sh -c 'echo \"deb http://download.virtualbox.org/virtualbox/debian $(lsb_release -cs) contrib\" >> /etc/apt/sources.list.d/virtualbox.list'")
-	os.system("wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -")
-	os.system("sudo apt-get update; sudo apt-get install -y virtualbox-5.0 apt-transport-https ca-certificates python-pip git")
+	os.system("sudo apt-get update; sudo apt-get install -y apt-transport-https ca-certificates python-pip git")
         os.system("sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D")
         os.system("sudo touch /etc/apt/sources.list.d/docker.list; sudo chmod 777 /etc/apt/sources.list.d/docker.list;  echo \"%s\" > /etc/apt/sources.list.d/docker.list" % deb)
 	os.system("sudo apt-get update; sudo apt-get install docker-engine -y; sudo gpasswd -a %s docker" % args.linux_username)
 	os.system("echo \"DOCKER_OPTS=\\\"-H tcp://127.0.0.1:2375 -H unix:///var/run/docker.sock\\\"\" | sudo tee /etc/default/docker > /dev/null")
 	os.system("sudo service docker restart; sudo pip install docker-compose")
-	os.system("sudo curl -L https://github.com/docker/machine/releases/download/v0.6.0/docker-machine-`uname -s`-`uname -m` > /usr/local/bin/docker-machine && sudo chmod +x /usr/local/bin/docker-machine")
 	os.system("git clone https://%s@bitbucket.org/authdash/authoriti-dashboard-environment.git ~/authoriti/authoriti_env" % args.bitbucket_username)
 	os.system("git clone https://%s@bitbucket.org/authdash/authoriti-dashboard.git ~/authoriti/authoriti_dash" % args.bitbucket_username)
-	os.system("cd ~/authoriti/authoriti_env; docker-machine create -d virtualbox dev; docker-machine regenerate-certs dev;  eval $(docker-machine env dev)")
-	os.system("docker-compose build; docker-compose up -d")
+	os.system("cd ~/authoriti/authoriti_env; docker-compose build; docker-compose up -d")
